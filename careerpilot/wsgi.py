f12a2dc03900f57a3ad053899328f78c70badbe4
kashyap3037-crypto <kashyap3037@gmail.com>
2026-07-23 14:37:09 +0530

    Configure project for Vercel deployment

--- careerpilot/wsgi.py
+++ careerpilot/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'careerpilot.settings')
 
 application = get_wsgi_application()
+
+# Expose app for Vercel Serverless
+app = application
