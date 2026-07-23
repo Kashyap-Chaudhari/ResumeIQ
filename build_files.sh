bf34adc66e7055dc66a947264ec43fad4942bec9
kashyap3037-crypto <kashyap3037@gmail.com>
2026-07-23 14:58:00 +0530

    Fix Vercel build script python version command

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "BUILD START"
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput --clear
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
 echo "BUILD END"
